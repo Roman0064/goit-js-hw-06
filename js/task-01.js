10d1d88065a892a92d85f3206db8ddc129b12db0
Roman0064 <alee93474@gmail.com>
2023-05-10 23:50:55 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,9 @@
 const numberOfCategories = document.querySelector("#categories");
+const category = numberOfCategories.querySelectorAll(".item");
+console.log(`Number of categories: ${category.length}`);
+
+category.forEach((categ) => {
+    const catName = categ.querySelector('h2').textContent;
+    const catValue = categ.querySelectorAll("li").length;
+    console.log(`Category: ${catName}\n Elements: ${catValue}`);
+})
