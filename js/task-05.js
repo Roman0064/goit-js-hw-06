3aacef32225fa939af02690af810616edd7b83d9
Roman0064 <alee93474@gmail.com>
2023-05-11 22:27:10 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const nameInputEl = document.querySelector("#name-input");
+const nameOutputEl = document.querySelector("#name-output");
+
+nameInputEl.addEventListener("input", () => {
+    const name = nameInputEl.value.trim() || "Anonymous";
+    nameOutputEl.textContent = name;
+});
