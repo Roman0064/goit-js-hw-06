f25c8c1500020d2dbb493c8b514ea3f09196e329
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-05-12 12:24:16 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginFormEl = document.querySelector(".login-form");
 
+loginFormEl.addEventListener("submit", formCallback);
+
+function formCallback(event){
+    event.preventDefault();
+
+    const email = loginFormEl.elements.email.value;
+    const password = loginFormEl.elements.password.value;
+
+    if(email === "" || password === "" ){
+        alert("Заповніть будьласка всі поля!!!");
+        return;
+    }
+    const dataLogin = {
+        email,
+        password,
+    };
+
+    console.log(dataLogin);
+
+    loginFormEl.reset();
+};
