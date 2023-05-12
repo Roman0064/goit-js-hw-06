8ead2c2763a6da1cae18c02638e83d116099ba1b
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-05-12 12:42:15 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const backgroundColorEl = document.querySelector(".color");
+const changeColorEl = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+
+function addColor(){
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  backgroundColorEl.textContent = randomColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+changeColorEl.addEventListener("click", addColor);
